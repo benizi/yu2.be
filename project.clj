0796d2eae50beed809642c9a191cbaa9843713ac
Benjamin R. Haskell <yu2.be@benizi.com>
2012-04-15 23:38:36 -0400

    Don't use raw HTML

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "yu2.be URL shortener site"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [ring/ring-jetty-adapter "1.0.1"]
-                 [compojure "1.0.2"]])
+                 [compojure "1.0.2"]
+                 [hiccup "1.0.0-RC2"]])
--- src/yu2/be.clj
+++ src/yu2/be.clj
@@ -2,6 +2,7 @@
   (:use [compojure.core])
   (:require [compojure.route :as route]
             [compojure.handler :as handler])
+  (:use [hiccup.core])
   (:use [ring.adapter.jetty :only [run-jetty]])
   (:use [ring.util.response :as response]))
 
@@ -11,9 +12,23 @@
   (GET "/" [] (response/file-response "index.html" {:root "public"}))
   (GET ["/:id", :id youtube-id] [id]
        (response/redirect (str "http://youtube.com/watch?v=" id)))
-  (GET "/*" {params :params}
+  (GET "/*" {params :params
+             {host "host"} :headers}
        (if (params :v)
-         (str "<html><body onload=\"document.getElementById('url').focus()\"><form><label for=\"url\">Short URL:</label><input id=\"url\" type=\"text\" value=\"http://yu2.be/" (params :v) "\"/></form><div><a href=\"/\">What is this?</a></div></body></html>")
+         (let [short-url (str "http://" host "/" (params :v))]
+           (html
+             [:html
+              [:head
+               [:title "YouTube short URL - yu2.be"]]
+              [:body {:onload "document.getElementById('url').focus()"}
+               [:form
+                [:label {:for "url"} "Short URL"]
+                [:input {:id "url"
+                         :type "text"
+                         :size (.length short-url)
+                         :value short-url}]]
+               [:div
+                [:a {:href "/"} "What is this?"]]]]))
          "Could not determine video from URL"))
   (route/not-found "Page not found"))
 
