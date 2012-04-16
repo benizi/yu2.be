30e2716e5a2c56207762afe7875e462d4762b384
Benjamin R. Haskell <yu2.be@benizi.com>
2012-04-16 01:12:16 -0400

    Make responses more modular

--- src/yu2/be.clj
+++ src/yu2/be.clj
@@ -8,29 +8,40 @@
 
 (def youtube-id #"[A-Za-z0-9\-._]{11}")
 
+(defn vid [id]
+  (response/redirect (str "http://youtube.com/watch?v=" id)))
+
+(defn shortener [id & options]
+  "An HTML page showing the shortened URL for the given ID"
+  (let [host (or (:host (first options)) "yu2.be")
+        short-url (str "http://" host "/" id)]
+    (html
+      [:html
+       [:head
+        [:title "YouTube short URL - yu2.be"]]
+       [:body {:onload "document.getElementById('url').focus()"}
+        [:form
+         [:label {:for "url"} "Short URL"]
+         [:input {:id "url"
+                  :type "text"
+                  :size (.length short-url)
+                  :value short-url}]]
+        [:div
+         [:a {:href "/"} "What is this?"]]]])))
+
 (defroutes yu2be
-  (GET "/" [] (response/file-response "index.html" {:root "public"}))
-  (GET ["/:id", :id youtube-id] [id]
-       (response/redirect (str "http://youtube.com/watch?v=" id)))
+  (GET "/" {params :params}
+       (if (params :v)
+         :next
+         (response/file-response "index.html" {:root "public"})))
+  (GET ["/:id" :id youtube-id] [id] (vid id))
+  (GET ["/:short/:id" :short #"[ev]" :id youtube-id] [_ id] (shortener id))
   (GET "/*" {params :params
              {host "host"} :headers}
        (if (params :v)
-         (let [short-url (str "http://" host "/" (params :v))]
-           (html
-             [:html
-              [:head
-               [:title "YouTube short URL - yu2.be"]]
-              [:body {:onload "document.getElementById('url').focus()"}
-               [:form
-                [:label {:for "url"} "Short URL"]
-                [:input {:id "url"
-                         :type "text"
-                         :size (.length short-url)
-                         :value short-url}]]
-               [:div
-                [:a {:href "/"} "What is this?"]]]]))
-         "Could not determine video from URL"))
-  (route/not-found "Page not found"))
+         (shortener (params :v) {:host host})
+         :next))
+  (route/not-found "Could not determine video from URL"))
 
 (defn -main [port]
   (run-jetty (handler/api yu2be) {:port (Integer. port)}))
